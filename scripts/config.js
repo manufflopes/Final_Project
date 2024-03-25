9bb77bde3981fca87876ed5bca70bd452d16267c
Manu <manuellafflopes@gmail.com>
2024-03-24 20:33:32 -0600

    config update

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,9 @@
-// export const apiBaseUrl = "https://opendesks-api.onrender.com/";
-export const apiBaseUrl = "http://localhost:3000/";
-
 //LOCAL
-export const rootDir = "/pages/";
-export const baseUrl = `http://opendesks.local:5500${rootDir}`;
+// export const rootDir = "/pages/";
+// export const baseUrl = `http://opendesks.local:5500${rootDir}`;
+// export const apiBaseUrl = "http://localhost:3000/";
 
 //PRD
-// export const rootDir = "/Final_Project/pages/";
-// export const baseUrl = `https://manufflopes.github.io${rootDir}`;
+export const rootDir = "/Final_Project/pages/";
+export const baseUrl = `https://manufflopes.github.io${rootDir}`;
+export const apiBaseUrl = "https://opendesks-api.onrender.com/";
